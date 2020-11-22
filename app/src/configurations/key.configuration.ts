e50bf7dd4fbea13732cc186b69d30c3602bf1043
MD-AZMAL <azmalmd12@gmail.com>
2020-11-22 18:56:54 +0530

    key configuration

---
+++ app/src/configurations/key.configuration.ts
@@ -0,0 +1,12 @@
+export const keyMapping = {
+    YAW_LEFT: 'q',
+    YAW_RIGHT: 'e',
+    PITCH_FORWARD: 'w',
+    PITCH_BACKWARD: 's',
+    ROLL_LEFT: 'a',
+    ROLL_RIGHT: 'd',
+    THROTTLE_UP: 'arrowup',
+    THROTTLE_DOWN: 'arrowdown',
+    ARM: 'enter',
+    DISARM: 'backspace'
+};
