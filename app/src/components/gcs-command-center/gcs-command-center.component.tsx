726cce4f1ae82ba948afd94694a5f87f4310caf3
MD-AZMAL <azmalmd12@gmail.com>
2020-11-22 18:54:28 +0530

    gcs command center component

--- app/src/components/gcs-command-center/gcs-command-center.component.tsx
+++ app/src/components/gcs-command-center/gcs-command-center.component.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import GcsMap from '../gcs-map/gcs-map.component'
 import Stack from '../stack/stack.component';
 import Telemetry from '../telemetry/telemetry.component';
+import Controller from '../controller/controller.component';
 import StackChild from '../stack-child/stack-child.component';
 
 const GcsCommandCenter = () => {
     return (
         <Stack>
             <StackChild Component={GcsMap} position='relative' />
-            <StackChild Component={Telemetry} right={15} top={15} />
+            <StackChild Component={Telemetry} position='absolute' right={15} top={15} />
+            <StackChild Component={Controller}/>
         </Stack>
     )
-}
+};
 
 export default GcsCommandCenter;
