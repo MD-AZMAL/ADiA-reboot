b77d1c7015a5177574313bc985d0ce4f95307773
MD-AZMAL <azmalmd12@gmail.com>
2020-11-22 18:55:43 +0530

    stack child component

--- app/src/components/stack-child/stack-child.component.tsx
+++ app/src/components/stack-child/stack-child.component.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const StackChild = ({Component,position,top,bottom,left,right}: any) => {
+const StackChild = ({Component,position,top,bottom,left,right,zIndex,width}: any) => {
 
     const styleObj = {
-        position: position ? position :  'absolute',
+        position: position ? position :  undefined,
         top: top ? top : undefined,
         left: left ? left : undefined,
         bottom: bottom ? bottom : undefined,
         right: right ? right : undefined,
+        width: width ? width : undefined,
+        zIndex: zIndex ? zIndex : undefined,
     };
-
-    console.log(styleObj)
 
     return (
         <div style={styleObj}>
