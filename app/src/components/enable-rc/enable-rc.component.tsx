3b226b381d92bf609ce28e4406cb56d34119d970
MD-AZMAL <azmalmd12@gmail.com>
2020-11-22 18:53:44 +0530

    enable rc button

--- app/src/components/enable-rc/enable-rc.component.tsx
+++ app/src/components/enable-rc/enable-rc.component.tsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React from 'react';
 import { Button } from 'rsuite';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectIsConnected } from '../../redux/fc/fc.selectors';
+import { disableRc, enableRc } from '../../redux/fc/fc.actions';
+import {
+  selectIsConnected,
+  selectIsRcEnabled,
+} from '../../redux/fc/fc.selectors';
 
-const EnableRC = ({isConnected} :any) => {
-    return (
-        <Button appearance='primary' disabled={!isConnected} >Enable RC</Button>
-    )
-}
+
+const EnableRC = ({ isConnected, isRcEnabled, disableRc, enableRc }: any) => {
+
+
+  const handleRcEnable = (event: any) => {
+    if (isRcEnabled) {
+      // disarm and disable
+      console.log('IN RC DISABLE');
+
+      disableRc();
+    } else {
+      // enable rc only
+      console.log('IN RC ENABLE');
+
+      enableRc();
+    }
+    event.target.blur(); // deselect after click
+  };
+
+  return (
+    <Button
+      appearance="primary"
+      disabled={!isConnected}
+      color={isRcEnabled ? 'red' : 'blue'}
+      onClick={handleRcEnable}
+    >
+      {isRcEnabled ? 'Disable RC' : 'Enable RC'}
+    </Button>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-    isConnected: selectIsConnected
+  isConnected: selectIsConnected,
+  isRcEnabled: selectIsRcEnabled,
 });
 
-export default connect(mapStateToProps)(EnableRC);
+const mapDispatchToProps = (dispatch: any) => ({
+  enableRc: () => dispatch(enableRc()),
+  disableRc: () => dispatch(disableRc()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EnableRC);
