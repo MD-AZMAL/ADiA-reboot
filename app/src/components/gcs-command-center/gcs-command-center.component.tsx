c5520a66da4d1625833dcbb90ba9f646b793b7c1
MD-AZMAL <azmalmd12@gmail.com>
2020-11-23 23:25:56 +0530

    updated video

--- app/src/components/gcs-command-center/gcs-command-center.component.tsx
+++ app/src/components/gcs-command-center/gcs-command-center.component.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import GcsMap from '../gcs-map/gcs-map.component'
+import React from 'react';
+// import { Container } from 'rsuite';
+// import Webcam from 'react-webcam';
+
+import GcsMap from '../gcs-map/gcs-map.component';
 import Stack from '../stack/stack.component';
 import Telemetry from '../telemetry/telemetry.component';
 import Controller from '../controller/controller.component';
 import StackChild from '../stack-child/stack-child.component';
+import DroneVideo from '../drone-video/drone-video.component';
 
 const GcsCommandCenter = () => {
-    return (
-        <Stack>
-            <StackChild Component={GcsMap} position='relative' />
-            <StackChild Component={Telemetry} position='absolute' right={15} top={15} />
-            <StackChild Component={Controller}/>
-        </Stack>
-    )
+  return (
+    <Stack>
+      <StackChild Component={GcsMap} position="relative" />
+      <StackChild
+        Component={Telemetry}
+        position="absolute"
+        right={15}
+        top={15}
+      />
+      <StackChild
+        Component={DroneVideo}
+        position="absolute"
+        top={0}
+        bottom={0}
+        right={0}
+        left={0}
+        zIndex={2}
+      />
+      <StackChild Component={Controller} zIndex={3}/>
+    </Stack>
+  );
 };
 
 export default GcsCommandCenter;
+
