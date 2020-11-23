1cbf98eb2b55b6e3cbd8e00768b93479165d126d
MD-AZMAL <azmalmd12@gmail.com>
2020-11-23 23:26:51 +0530

    added stack child props update;

--- app/src/components/stack-child/stack-child.component.tsx
+++ app/src/components/stack-child/stack-child.component.tsx
@@ -4,10 +4,10 @@ const StackChild = ({Component,position,top,bottom,left,right,zIndex,width}: any
 
     const styleObj = {
         position: position ? position :  undefined,
-        top: top ? top : undefined,
-        left: left ? left : undefined,
-        bottom: bottom ? bottom : undefined,
-        right: right ? right : undefined,
+        top: top || top == 0 ? top : undefined,
+        left: left || left == 0 ? left : undefined,
+        bottom: bottom || bottom == 0 ? bottom : undefined,
+        right: right || right === 0 ? right : undefined,
         width: width ? width : undefined,
         zIndex: zIndex ? zIndex : undefined,
     };
