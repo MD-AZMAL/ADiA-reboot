55f6dd21dded40193504ed1d6b97cdae1cdd1909
MD-AZMAL <azmalmd12@gmail.com>
2020-11-24 10:26:52 +0530

    enable video stack options

--- app/src/components/gcs-command-center/gcs-command-center.component.tsx
+++ app/src/components/gcs-command-center/gcs-command-center.component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 // import { Container } from 'rsuite';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
 // import Webcam from 'react-webcam';
 
 import GcsMap from '../gcs-map/gcs-map.component';
@@ -8,8 +10,9 @@ import Telemetry from '../telemetry/telemetry.component';
 import Controller from '../controller/controller.component';
 import StackChild from '../stack-child/stack-child.component';
 import DroneVideo from '../drone-video/drone-video.component';
+import { selectIsVideoEnabled } from '../../redux/video/video.selectors';
 
-const GcsCommandCenter = () => {
+const GcsCommandCenter = ({isVideoEnabled}: any) => {
   return (
     <Stack>
       <StackChild Component={GcsMap} position="relative" />
@@ -18,6 +21,7 @@ const GcsCommandCenter = () => {
         position="absolute"
         right={15}
         top={15}
+        zIndex={3}
       />
       <StackChild
         Component={DroneVideo}
@@ -26,12 +30,16 @@ const GcsCommandCenter = () => {
         bottom={0}
         right={0}
         left={0}
-        zIndex={2}
+        zIndex={isVideoEnabled ? 2 : -1}
       />
       <StackChild Component={Controller} zIndex={3}/>
     </Stack>
   );
 };
 
-export default GcsCommandCenter;
+const mapStateToProps = createStructuredSelector({
+  isVideoEnabled: selectIsVideoEnabled
+});
+
+export default connect(mapStateToProps)(GcsCommandCenter);
 
