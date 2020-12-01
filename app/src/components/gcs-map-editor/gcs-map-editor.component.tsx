1c56bf0dc5975eda31956f096ebef99c952424ee
MD-AZMAL <azmalmd12@gmail.com>
2020-12-01 15:44:15 +0530

    calculate point cloud

--- app/src/components/gcs-map-editor/gcs-map-editor.component.tsx
+++ app/src/components/gcs-map-editor/gcs-map-editor.component.tsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import { Editor, DrawPolygonMode, EditingMode } from 'react-map-gl-draw';
-import GcsMapEditorControls from '../gcs-map-editor-controls/gcs-map-editor-controls.component';
+import { connect } from 'react-redux';
 import turf from 'turf';
+
+import {
+  setWayPointPointCloud,
+  setWayPointPointCloudNull,
+} from '../../redux/waypoint/waypoint.actions';
+import GcsMapEditorControls from '../gcs-map-editor-controls/gcs-map-editor-controls.component';
 
 import { getFeatureStyle, getEditHandleStyle } from './gcs-map-editor.styles';
 
-const GcsMapEditor = () => {
+const GcsMapEditor = ({
+  setWayPointPointCloud,
+  setWayPointPointCloudNull,
+}: any) => {
   const [selectedFeatureIndex, setSelectedFeatureIndex] = useState(null);
-  const [pointGrid, setPointGrid] = useState<any>(null);
   const [mode, setMode] = useState<any>(null);
   let editorRef: any = null;
 
   const onDraw = () => {
-    if(editorRef.getFeatures().length === 0) {
-        setMode(new DrawPolygonMode());
+    if (editorRef.getFeatures().length === 0) {
+      setMode(new DrawPolygonMode());
     }
   };
 
@@ -35,34 +43,39 @@ const GcsMapEditor = () => {
     const selectedIndex = selectedFeatureIndex;
     if (selectedIndex !== null && selectedIndex >= 0) {
       editorRef.deleteFeatures(selectedIndex);
+      setWayPointPointCloudNull();
     }
-    setPointGrid(null);
   };
 
   const onMakePointCloud = () => {
-    console.log(mode);
-    // console.log('up------up');
-    // console.log(editorRef.getFeatures());
-    // console.log('up-----up');
-    let test = turf.bbox(editorRef.getFeatures()[0]);
-    console.log(test);
-    console.log(editorRef);
-    let testPoly = turf.bboxPolygon(test);
-    editorRef.addFeatures(testPoly);
-    // console.log('aaa---------aaaa');
-    // console.log(editorRef.getFeatures());
-    // console.log('aaa---------aaaa');
+    if (editorRef.getFeatures().lenth !== 0) {
+      // console.log(mode);
+      // console.log('up------up');
+      // console.log(editorRef.getFeatures());
+      // console.log('up-----up');
+      let boundingBoxFromPolygon = turf.bbox(editorRef.getFeatures()[0]);
+      // console.log(test);
+      // console.log(editorRef);
+      // let testPoly = turf.bboxPolygon(test);
+      // editorRef.addFeatures(testPoly);
+      // console.log('aaa---------aaaa');
+      // console.log(editorRef.getFeatures());
+      // console.log('aaa---------aaaa');
 
-    let test2 = turf.pointGrid(test, 10, 'meters');
-    setPointGrid(test2);
+      let pointCloudFromBoundingBox = turf.pointGrid(
+        boundingBoxFromPolygon,
+        10,
+        'meters'
+      );
+      setWayPointPointCloud(pointCloudFromBoundingBox);
 
-    // console.log(editorRef.getFeatures());
+      // console.log(editorRef.getFeatures());
+    }
   };
 
   const onExecuteMission = () => {
     console.log(editorRef.getFeatures());
   };
-
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
@@ -87,4 +100,10 @@ const GcsMapEditor = () => {
   );
 };
 
-export default GcsMapEditor;
+const mapDispatchToProps = (dispatch: any) => ({
+  setWayPointPointCloud: (pointCloud: any) =>
+    dispatch(setWayPointPointCloud(pointCloud)),
+  setWayPointPointCloudNull: () => dispatch(setWayPointPointCloudNull()),
+});
+
+export default connect(null, mapDispatchToProps)(GcsMapEditor);
