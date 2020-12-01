e93bb65d50810f0f0037832e0378a3d88df997ea
MD-AZMAL <azmalmd12@gmail.com>
2020-12-01 15:44:43 +0530

    main map component to point cloud

--- app/src/components/gcs-map/gcs-map.component.tsx
+++ app/src/components/gcs-map/gcs-map.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL from 'react-map-gl';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
@@ -8,8 +8,9 @@ import { selectMapStyle } from '../../redux/map-configuration/map-configuration.
 import { selectGcsExpanded } from '../../redux/gcs-sidebar/gcs-sidebar.selectors';
 
 import GcsMapEditor from '../gcs-map-editor/gcs-map-editor.component';
+import GcsPointCloudComponent from '../gcs-point-cloud/gcs-point-cloud.component';
 
-const GcsMap = ({ expanded, mapStyle, mode, setWayPointMode }: any) => {
+const GcsMap = ({ expanded, mapStyle }: any) => {
 
   const width = expanded ? `calc(100vw - 260px)` : `calc(100vw - 56px)`;
   const height = 'calc(100vh - 56px)';
@@ -19,10 +20,6 @@ const GcsMap = ({ expanded, mapStyle, mode, setWayPointMode }: any) => {
     longitude: 85.8829895,
     zoom: 15,
   });
-
-  const displayViewPort = () => {
-    console.log(viewport);
-  };
 
   return (
     <ReactMapGL
@@ -36,18 +33,9 @@ const GcsMap = ({ expanded, mapStyle, mode, setWayPointMode }: any) => {
       onViewportChange={(nextViewport) => {
         setViewport(nextViewport);
       }}
-      // onClick={(nextViewport) => {
-      //   console.log(nextViewport.lngLat);
-      // }}
     >
       <GcsMapEditor  />
-      {/* {
-        pointGrid && pointGrid.features.map((point: any, key: number) => (
-          <Marker offsetLeft={-5} offsetTop={-15} key={key} latitude={point.geometry.coordinates[1]} longitude={point.geometry.coordinates[0]}>
-            <Icon icon="circle" style={{color: '#fcae22'}}  />
-          </Marker>
-        ))
-      } */}
+      <GcsPointCloudComponent />
     </ReactMapGL>
   );
 };
